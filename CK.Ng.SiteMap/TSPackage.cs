e5d1a47f80a7b61d1e35b1db1f08b9f2bd7a0cd9
Romain Delorme-Glorieux <romain.delorme-glorieux@signature.one>
2026-05-11 18:32:48 +0200

    Update dependencies

--- CK.Ng.SiteMap/TSPackage.cs
+++ CK.Ng.SiteMap/TSPackage.cs
@@ -1,16 +1,12 @@
 using CK.Core;
 using CK.TypeScript;
-using Microsoft.AspNetCore.Antiforgery;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CK.Ng.SiteMap;
 
 [TypeScriptPackage]
 [Requires<CK.Ng.AspNet.Auth.AspNetAuthPackage>]
+[Requires<CK.Ng.Zorro.ActionBarComponent>]
+[Requires<CK.Ng.Zorro.BreadcrumbComponent>]
 public sealed class TSPackage : TypeScriptPackage
 {
 }
