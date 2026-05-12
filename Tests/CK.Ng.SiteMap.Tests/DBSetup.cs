16518e4fb214c163f30b091dd3841ddaf08fb3d2
Olivier Spinelli <olivier.spinelli@signature-code.com>
2026-05-12 16:38:51 +0200

    Fixed and setup test (with DB).

---
+++ Tests/CK.Ng.SiteMap.Tests/DBSetup.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CK.Ng.SiteMap.Tests;
+
+[TestFixture]
+public class DBSetup : CK.DB.Tests.DBSetup
+{
+}
