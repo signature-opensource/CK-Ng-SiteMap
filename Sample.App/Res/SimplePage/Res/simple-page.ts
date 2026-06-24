dc6c0a230b5a2157d65e8354a72b4fcbb4f8a960
Romain Delorme-Glorieux <romain.delorme-glorieux@signature.one>
2026-06-24 21:28:33 +0200

    Add SimplePageComponent

---
+++ Sample.App/Res/SimplePage/Res/simple-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-simple-page',
+    imports: [],
+    templateUrl: './simple-page.html'
+})
+export class SimplePage {
+}
---
+++ Sample.App/Res/SimplePage/SimplePageComponent.cs
@@ -0,0 +1,8 @@
+using CK.TS.Angular;
+
+namespace Sample.App.Res.SimplePage;
+
+[NgComponent]
+public sealed class SimplePageComponent : NgComponent, INgNamedComponent
+{
+}
